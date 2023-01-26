746f82c99bed23d642ee6c8eee97d499a7d61c00
sas-adilis <contact@adilis.fr>
2023-01-26 06:48:04 +0100

    fo design

--- cartassociations.php
+++ cartassociations.php
@@ -1,28 +1,8 @@
 <?php
-/**
- * 2007-2015 PrestaShop
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2015 PrestaShop SA
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
- */
+use PrestaShop\PrestaShop\Adapter\Image\ImageRetriever;
+use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
+use PrestaShop\PrestaShop\Core\Product\ProductListingPresenter;
+use PrestaShop\PrestaShop\Adapter\Product\ProductColorsRetriever;
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -44,6 +24,10 @@ class CartAssociations extends Module
         $this->displayName = $this->l('Cart associations');
         $this->description = $this->l('Add associations to cart');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall, all datas will be lost?');
+
+        if (!$this->isHookInstalledInTemplateFile()) {
+            $this->warning = $this->l('Please install hook in cart-detailed.tpl');
+        }
     }
     
     public function install()
@@ -54,7 +38,9 @@ class CartAssociations extends Module
         return
             parent::install() &&
             $this->registerHook('displayAdminProductsExtra') &&
-            $this->registerHook('actionProductUpdate');
+            $this->registerHook('actionProductUpdate') &&
+            $this->registerHook('displayCartAssociations') &&
+            $this->registerHook('displayHeader');
     }
     
     public function uninstall()
@@ -62,9 +48,13 @@ class CartAssociations extends Module
         if (file_exists($this->getLocalPath() . 'sql/uninstall.php')) {
             require_once($this->getLocalPath() . 'sql/uninstall.php');
         }
-        parent::uninstall();
+        return parent::uninstall();
     }
 
+    /**
+     * @param $params
+     * @return void|string
+     */
     public function hookDisplayAdminProductsExtra($params) {
         $id_product = (int)$params['id_product'];
         if (!$id_product) {
@@ -103,7 +93,10 @@ class CartAssociations extends Module
         return $this->display(__FILE__, 'admin-product-tab.tpl');
     }
 
-
+    /**
+     * @param $params
+     * @return void
+     */
     public function hookActionProductUpdate($params)
     {
         $id_product = $params['id_product'];
@@ -124,5 +117,124 @@ class CartAssociations extends Module
             }
             Db::getInstance()->insert('cart_association', $datas_to_insert, false, true, Db::REPLACE);
         }
+    }
+
+    private function isHookInstalledInTemplateFile(): bool
+    {
+        $file = _PS_THEME_DIR_.'templates/checkout/_partials/cart-detailed.tpl';
+        if (!file_exists($file)) {
+            $file = _PS_PARENT_THEME_DIR_.'templates/checkout/_partials/cart-detailed.tpl';
+        }
+
+        if (!file_exists($file)) {
+            return false;
+        }
+
+        $content = file_get_contents($file);
+        return $content && strpos($content, 'displayCartAssociations') !== false;
+    }
+
+    public function hookDisplayCartAssociations($params) {
+        $cache_id = 'cart_associations|'.$this->context->cart->id;
+        if (!Cache::retrieve($cache_id)) {
+            $products_in_cart = $cart_associations = [];
+            foreach ($this->context->cart->getProducts() as $product_in_cart) {
+                $products_in_cart[] = $product_in_cart['id_product'];
+            }
+
+            $now = date('Y-m-d') . ' 00:00:00';
+            $nb_days_new_product = (int) Configuration::get('PS_NB_DAYS_NEW_PRODUCT');
+            $id_lang = $this->context->cookie->id_lang;
+            $id_shop = $this->context->shop->id;
+
+            $sql = new DbQuery();
+            $sql->select('
+                ca.id_product_1, p.*, product_shop.*, stock.out_of_stock, IFNULL(stock.quantity, 0) as quantity,
+                pl.`description`, pl.`description_short`, pl.`link_rewrite`, pl.`meta_description`,
+                pl.`meta_keywords`, pl.`meta_title`, pl.`name`, pl.`available_now`, pl.`available_later`,
+                image_shop.`id_image` id_image, il.`legend`, m.`name` AS manufacturer_name,
+                (DATEDIFF(product_shop.`date_add`,
+                    DATE_SUB(
+                        "' . $now . '",
+                        INTERVAL ' . $nb_days_new_product . ' DAY
+                    )
+                ) > 0) as new'
+            );
+            $sql->from('cart_association',  'ca');
+            $sql->innerJoin('product', 'p', 'p.id_product = ca.id_product_2');
+            $sql->join(Shop::addSqlAssociation('product', 'p'));
+            $sql->leftJoin(
+                'product_lang',
+                'pl',
+                'p.`id_product` = pl.`id_product` AND pl.`id_lang` = ' . (int)$id_lang . Shop::addSqlRestrictionOnLang('pl')
+            );
+            $sql->leftJoin('image_shop', 'image_shop', 'image_shop.`id_product` = p.`id_product` AND image_shop.cover=1 AND image_shop.id_shop=' . (int)$id_shop);
+            $sql->leftJoin('image_lang', 'il', 'image_shop.`id_image` = il.`id_image` AND il.`id_lang` = ' . (int)$id_lang);
+            $sql->leftJoin('manufacturer', 'm', 'm.`id_manufacturer` = p.`id_manufacturer`');
+            $sql->where('ca.id_product_1 IN ('.implode(',', $products_in_cart).')');
+            $sql->where('ca.id_product_2 NOT IN ('.implode(',', $products_in_cart).')');
+            $sql->where('product_shop.`active` = 1');
+            $sql->where('product_shop.`visibility` IN ("both", "catalog")');
+
+            if (Group::isFeatureActive()) {
+                $groups = FrontController::getCurrentCustomerGroups();
+                $sql->where('
+                    EXISTS(SELECT 1 FROM `' . _DB_PREFIX_ . 'category_product` cp
+                    JOIN `' . _DB_PREFIX_ . 'category_group` cg ON (cp.id_category = cg.id_category AND cg.`id_group` ' . (count($groups) ? 'IN (' . implode(',', $groups) . ')' : '=' . (int) Group::getCurrent()->id) . ')
+                    WHERE cp.`id_product` = p.`id_product`)'
+                );
+            }
+
+            if (Combination::isFeatureActive()) {
+                $sql->select('product_attribute_shop.minimal_quantity AS product_attribute_minimal_quantity, IFNULL(product_attribute_shop.id_product_attribute,0) id_product_attribute');
+                $sql->leftJoin('product_attribute_shop', 'product_attribute_shop', 'p.`id_product` = product_attribute_shop.`id_product` AND product_attribute_shop.`default_on` = 1 AND product_attribute_shop.id_shop=' . (int)$id_shop);
+            }
+            $sql->join(Product::sqlStock('p', 0));
+            $sql->orderBy('ca.position ASC');
+
+            $products = Db::getInstance()->executeS($sql);
+
+            if (count($products)) {
+                $presenterFactory = new ProductPresenterFactory(Context::getContext());
+                $presentationSettings = $presenterFactory->getPresentationSettings();
+
+                $presenter = new ProductListingPresenter(
+                    new ImageRetriever(
+                        $this->context->link
+                    ),
+                    $this->context->link,
+                    new PriceFormatter(),
+                    new ProductColorsRetriever(),
+                    $this->context->getTranslator()
+                );
+
+                foreach ($products as $product) {
+                    if (!isset($cart_associations[$product['id_product_1']])) {
+                        $cart_associations[$product['id_product_1']] = [];
+                    }
+                    $cart_associations[$product['id_product_1']][] = $presenter->present(
+                        $presentationSettings,
+                        Product::getProductProperties($id_lang, $product, $this->context),
+                        $this->context->language
+                    );
+                }
+            }
+            Cache::store($cache_id, $cart_associations);
+        }
+
+        /** @var Product $product */
+        $product = $params['product'];
+
+        $cart_associations = Cache::retrieve($cache_id);
+        if (isset($cart_associations[$product->id])) {
+            $this->context->smarty->assign(array(
+                'cart_associations' => $cart_associations[$product->id]
+            ));
+            return $this->display($this->getLocalPath(), '/views/templates/hook/cart-associations.tpl');
+        }
+    }
+
+    public function hookDisplayHeader() {
+        $this->context->controller->registerStylesheet('modules-cartassociations', 'modules/'.$this->name.'/views/css/cartassociations.css', ['media' => 'all', 'priority' => 150]);
     }
 }
---
+++ views/templates/hook/cart-associations.tpl
@@ -0,0 +1,83 @@
+{foreach $cart_associations as $cart_association}
+    <li class="cart-item cart-item-association bg-light">
+
+        <div class="product-line-grid row align-items-center small-gutters">
+
+
+            <!--  product left body: description -->
+            <div class="product-line-grid-body col-12 col-sm-6 col-md-6">
+                <div class="row align-items-center small-gutters">
+                    <div class="col col-auto product-image">
+                        <a href="{$cart_association.url}">
+                            {if $cart_association.default_image}
+                                <img src="{$cart_association.default_image.bySize.cart_default.url}" alt="{$cart_association.name|escape:'quotes'}"  class="img-fluid" loading="lazy">
+                            {else}
+                                <img src="{$urls.no_picture_image.bySize.cart_default.url}" class="img-fluid"  loading="lazy" />
+                            {/if}
+                        </a>
+                    </div>
+                    <div class="col">
+                        <small class="product-advise text-primary">{l s="We recommand" mod="cart_associations"}</small>
+                        <div class="product-line-info">
+                            <a class="label" href="{$cart_association.url}">{$cart_association.name}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--  product left body: description -->
+            <div class="col-12 col-sm-6 col-md-6 product-line-grid-right product-line-actions">
+                <div class="row align-items-center small-gutters justify-content-end">
+                    <!--  product unit-->
+                    <div class="col col-auto col-md unit-price">
+                        {if $cart_association.has_discount}
+                            <span class="product-discount">
+                        <span class="regular-price">{$cart_association.regular_price}</span>
+                            {if $cart_association.discount_type === 'percentage'}
+                                <span class="discount discount-percentage mr-1">
+                                 -{$cart_association.discount_percentage_absolute}
+                                </span>
+                            {else}
+                            <span class="discount discount-amount mr-1">
+                                 -{$cart_association.discount_to_display}
+                            </span>
+                            {/if}
+                    </span>
+                        {/if}
+                        <span class="value">{$cart_association.price}</span>
+                        {if $cart_association.unit_price_full}
+                            <div class="unit-price-cart">{$cart_association.unit_price_full}</div>
+                        {/if}
+                        {hook h='displayProductPriceBlock' product=$cart_association type="unit_price"}
+                    </div>
+
+                    <div class="col col-auto col-md qty">
+                        <form action="{$urls.pages.cart}" method="post">
+                            <input type="hidden" name="id_product" value="{$cart_association.id}">
+                            <input type="hidden" name="token" value="{$static_token}">
+                            <input type="hidden" name="id_product_attribute" value="{$cart_association.id_product_attribute}">
+                            <div class="input-group-add-cart">
+                                <button
+                                        class="btn btn-product-list add-to-cart"
+                                        data-button-action="add-to-cart"
+                                        type="submit"
+                                        {if !$cart_association.add_to_cart_url}
+                                            disabled
+                                        {/if}
+                                >
+                                    {l s='Add' d='Shop.Theme.Actions'}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col col-auto col-md price">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div class="col col-auto"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </li>
+{/foreach}
